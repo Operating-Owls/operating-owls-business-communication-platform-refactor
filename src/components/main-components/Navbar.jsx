6a74672dcc84aa9c53fa1b85ca1ea1d73338f6ba
Benjamin Schoolland <bschoolland@gmail.com>
2024-05-08 10:48:07 -0700

    feat: copy Profile Component over

--- src/components/main-components/Navbar.jsx
+++ src/components/main-components/Navbar.jsx
@@ -1,18 +1,20 @@
-import * as React from 'react';
-import SearchBar from './Search';
+import * as React from "react";
+import SearchBar from "./Search";
+import ProfileComponent from './ProfileComponent';
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 
 const Navbar = () => {
-  const[searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
   const handleSearchOpen = (event) => {
-      setAnchorEl(event.currentTarget);
-      //setForcePopover(true);
-      console.log("reopening this popover")
+    setAnchorEl(event.currentTarget);
+    //setForcePopover(true);
+    console.log("reopening this popover");
   };
 
   return (
@@ -25,15 +27,24 @@ const Navbar = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-          >
-          </IconButton>
+          ></IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Operating Owls
           </Typography>
-          <SearchBar 
-              searchValue={searchValue} 
-              setSearchValue={setSearchValue} 
-              handleSearchOpen={handleSearchOpen} 
+          <SearchBar
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            handleSearchOpen={handleSearchOpen}
+          />
+          <ProfileComponent
+            username="username"
+            nickname="john doe"
+            online={true}
+            // FIXME: Replace with actual user data
+            // username={props.username}
+            // nickname={props.username}
+            // online={true}
+            pfp="https://picsum.photos/200"
           />
         </Toolbar>
       </AppBar>
